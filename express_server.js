f6131735c60b2e387d1ecab56bbcc821bd602939
Camron Naderi <camronnaderi57@gmail.com>
2023-05-28 20:32:20 -0700

    add a new route for single url

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index.ejs", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show.ejs", templateVars);
+});
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
