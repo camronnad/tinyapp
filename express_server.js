a1d4f12239f78063e66e5379114493fa4ee7c350
Camron Naderi <camronnaderi57@gmail.com>
2023-06-01 18:32:07 -0700

    Add a POST route for /register, and Update all endpoints that pass user_id

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+//Use for generating Ids 
+
 function generateRandomString() {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -10,15 +12,29 @@ function generateRandomString() {
   return result;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////////////////////////////////////////
 
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const express = require('express');
+
+////////////////////////////////////////////////////////////////////////////////
+// Configuration
+////////////////////////////////////////////////////////////////////////////////
+
 const app = express();
 const PORT = 8080;
+
+////////////////////////////////////////////////////////////////////////////////
+// Middleware
+////////////////////////////////////////////////////////////////////////////////
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan('dev'));
 app.use((req, res, next) => { 
   if (req.cookies["username"]) {
     req.username = req.cookies["username"]
@@ -26,61 +42,105 @@ app.use((req, res, next) => {
   next()
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// Database
+////////////////////////////////////////////////////////////////////////////////
+
 const urlDatabase = {
    "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "abc",
+    email: "a@a.com",
+    password: "123",
+  },
+  user2RandomID: {
+    id: "def",
+    email: "b@b.com",
+    password: "456",
+  },
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////////////////////////////////////////
+
+// SENDS "Hello!" to that URL
 app.get("/", (req, res) => {
   console.log("hello")
 res.send("Hello!");
 });
 
+//DISPLAYS the Register Form
 app.get("/register", (req,res) => {
   res.render("urls_register.ejs")
 })
 
+//ADDS New User Object and Redirects Us to the index page
+app.post("/register", (req, res) => {
+const id = generateRandomString();
+users[id] = {
+  id: id,
+  email: req.body.email,
+  password: req.body.password,
+}
+res.cookie("user_id", id)
+  res.redirect("/urls");
+})
+
+//
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls")
 })
 
+//
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user_id")
   res.redirect("/urls");
 })
 
+//
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("user_id", req.body.username);
   res.redirect("/urls");
 });
 
+//
 app.get("/urls/new", (req, res) => {
  res.render("urls_new.ejs");
 });
 
+//
 app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString()
 urlDatabase[shortUrl] = req.body.longURL
  res.redirect(`/urls/${shortUrl}`); 
 });
 
+//
 app.get("/u/:id", (req, res) => {
   const id = req.params.id
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
 
+//
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["user_id"] };
   res.render("urls_show.ejs", templateVars);
 });
 
+//
 app.post("/urls/:id/edit", (req, res) => {
   const shortUrl = req.params.id;
   const longUrl  = req.body.editUrl;
@@ -88,18 +148,25 @@ app.post("/urls/:id/edit", (req, res) => {
   res.redirect("/urls");
 }); 
 
+//
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"],
+        user: [req.cookies["user_id"]],
         urls: urlDatabase 
       };
   res.render("urls_index.ejs", templateVars);
 });
 
+//
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// Listener
+////////////////////////////////////////////////////////////////////////////////
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
